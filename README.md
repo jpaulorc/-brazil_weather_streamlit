845e1b9aa1eb07cb5811df5d7057d9a655fca34b
Joao Corte <jpaulorc@gmail.com>
2022-02-06 22:21:33 -0300

    First Commit

---
+++ README.md
@@ -0,0 +1,1 @@
+#Brazil Weather Data Visualization
---
+++ streamlit_app/app.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import streamlit as st
+
+
+def display_header():
+    """Used to display the header"""
+    st.title("Brazil Weather Data Visualization")
+    st.text("Brazil Weather Data Visualization")
+    st
+
+
+display_header()
