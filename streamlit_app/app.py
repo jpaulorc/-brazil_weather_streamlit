253897463367d7474aee587daf1d7c3f0a1bde68
Joao Corte <jpaulorc@gmail.com>
2022-02-08 23:42:40 -0300

    Welcome page added

--- streamlit_app/app.py
+++ streamlit_app/app.py
@@ -1,4 +1,4 @@
-import csv
+"""import csv
 import glob
 import itertools
 import os
@@ -11,20 +11,61 @@ import matplotlib.patches as mpatches
 import matplotlib.pyplot as plt
 import missingno as msno
 import numpy as np
-import pandas as pd
+
 import plotly.express as px
 import plotly.graph_objects as go
 import requests
-import seaborn as sns
+import seaborn as sns"""
+import pandas as pd
 import streamlit as st
-from branca.element import Figure
+from PIL import Image
+
+# from branca.element import Figure
+
+st.set_page_config(layout="wide")
 
 
 def display_header():
-    """Used to display the header"""
-    st.title("Brazil Weather Data Visualization")
-    st.text("Brazil Weather Data Visualization")
-    st
+
+    row0_0, row0_1 = st.columns((2, 1))
+
+    with row0_0:
+        st.title("Brazil Data Meteorological Visualization")
+        st.markdown(
+            "[![GitHub](https://badgen.net/badge/icon/GitHub?icon=github&color=black&label)](https://github.com/jpaulorc/brazil_weather_streamlit)"
+        )
+    with row0_1:
+        st.subheader(
+            "Project created to obtain information about the weather in Brazil, from 2000 to 2020."
+        )
+
+    st.markdown("---")
+
+    row1_0, row1_1, row1_2 = st.columns((3, 1, 2))
+
+    with row1_0:
+        st.markdown(
+            """
+                ### Data collected by [INMET](https://portal.inmet.gov.br/): National Institute of Meteorology.
+            """
+        )
+        st.image(Image.open("streamlit_app/img/INMET.png"))
+
+    row1_2.markdown(
+        """
+        ### Datasets:
+
+        [Complete Brazil Weather Database - 2000/2020](https://www.kaggle.com/gbofrc/complete-brazil-weather-database-20002020)
+
+        Data collected by meteorological stations of the National Institute of Meteorology - INMET, distributed in the Brazilian territory between 2000 and 2021.
+
+        [INMET Automatic Stations](https://portal.inmet.gov.br/paginas/catalogoaut)
+
+        Location from INMET weather stations.
+        """
+    )
+
+    st.markdown("---")
 
 
 display_header()
